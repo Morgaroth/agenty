a99ceebdf66853fa8d9530448f31fc9a901b36a8
Mateusz Jaje <mateuszjaje@gmail.com>
2015-06-10 23:56:04 +0200

    added cors

--- src/main/scala/io/github/morgaroth/agenty/api/Server.scala
+++ src/main/scala/io/github/morgaroth/agenty/api/Server.scala
@@ -6,10 +6,54 @@ import com.typesafe.config.ConfigFactory
 import spray.can.Http
 import spray.routing._
 
+
+import spray.http.HttpHeaders._
+import spray.http.HttpMethods._
+import spray.http.{AllOrigins, HttpMethod, HttpMethods, HttpResponse}
+import spray.routing._
+
+trait CORSSupport {
+  this: HttpService =>
+
+  def AllowOriginHeader = AllOrigins
+
+  val BasicAllowedHeaders: List[String] = List(
+    Origin.name, `Content-Type`.name, Accept.name,
+    `Accept-Encoding`.name, `Accept-Language`.name,
+    Host.name, `User-Agent`.name, "X-Requested-With",
+    "Referer"
+  )
+
+  /**
+   * Override when need to add more allowed headers
+   * @return
+   */
+  def AdditionalAllowedHeaders: List[String] = List.empty
+
+  val basicCORSHeaders = List(
+    `Access-Control-Allow-Headers`(BasicAllowedHeaders ::: AdditionalAllowedHeaders),
+    `Access-Control-Allow-Origin`(AllowOriginHeader),
+    `Access-Control-Max-Age`(1728000)
+  )
+
+  def cors[T]: Directive0 = mapRequestContext {
+    ctx => ctx.withRouteResponseHandling {
+      //It is an option request for a resource that responds to some other method
+      case Rejected(x) if ctx.request.method.equals(HttpMethods.OPTIONS) && x.exists(_.isInstanceOf[MethodRejection]) =>
+        val allowedMethods: List[HttpMethod] = x.filter(_.isInstanceOf[MethodRejection]).map(rejection => {
+          rejection.asInstanceOf[MethodRejection].supported
+        })
+        ctx.complete(HttpResponse().withHeaders(
+          `Access-Control-Allow-Methods`(OPTIONS, allowedMethods: _*) :: basicCORSHeaders
+        ))
+    }.withHttpResponseHeadersMapped(headers => `Access-Control-Allow-Origin`(AllowOriginHeader) :: headers)
+  }
+}
+
 trait serviceActor {
   this: WebApi =>
-  lazy val serviceActorProps = Props(new HttpServiceActor {
-    override def receive: Actor.Receive = runRoute(routes)
+  lazy val serviceActorProps = Props(new HttpServiceActor with CORSSupport{
+    override def receive: Actor.Receive = runRoute(cors(routes))
   })
 }
 
